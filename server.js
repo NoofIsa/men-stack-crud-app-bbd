94c2619878e872ca14372260afee7c408d79118b
Noof <202102651@stu.uob.edu.bh>
2025-07-14 09:48:25 +0300

    connect database

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//  server setup
+/* server setup
 const express = require('express');
 const app = express();
 const path = require ('path');
@@ -14,4 +14,28 @@ app.use(express.static(path.join(__dirname, "public")));
 app.listen(3000, () => {
     console.log('Listening on port 3000')
 });
+*/
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const path = require('path')
 
+// DATABASE CONNECTION
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
+
+// MIDDLEWARE
+app.use(express.static(path.join(__dirname, 'public')))
+
+// GET / (home)
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
